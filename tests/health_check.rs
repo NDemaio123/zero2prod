93a0651587e080f96d45f99f0f64f4505c14388d
Nicholas Demaio <ndemaio123@proton.me>
2024-12-15 17:44:35 +1000

    test(health_check): fix telemetry for tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,24 @@
 use sqlx::{Connection, Executor, PgConnection, PgPool};
-use std::net::TcpListener;
+use std::{net::TcpListener, sync::LazyLock};
 use uuid::Uuid;
 use zero2prod::{
     configuration::{get_configuration, DatabaseSettings},
     startup,
+    telemetry::{get_subscriber, init_subscriber},
 };
+
+static TRACING: LazyLock<()> = LazyLock::new(|| {
+    let subscriber_name = "test".to_string();
+    let default_filter_level = "info".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 pub struct TestApp {
     pub address: String,
@@ -12,14 +26,18 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    LazyLock::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
+
     let mut configuration = get_configuration().expect("Failed to read configuration.");
     configuration.database.database_name = Uuid::new_v4().to_string();
     let connection_pool = configure_database(&configuration.database).await;
+
     let server = startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
-    let _ = tokio::spawn(server);
+    tokio::spawn(server);
     TestApp {
         address,
         db_pool: connection_pool,
@@ -34,16 +52,20 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         port: config.port,
         host: config.host.clone(),
     };
+
     let mut connection = PgConnection::connect(&maintainence_settings.connection_string())
         .await
         .expect("Failed to connect to Postgres");
+
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create database.");
+
     let connection_pool = PgPool::connect(&config.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
+
     sqlx::migrate!("./migrations")
         .run(&connection_pool)
         .await
@@ -54,18 +76,15 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
 
 #[tokio::test]
 async fn health_check_works() {
-    // Arrange
     let address = spawn_app().await.address;
     let client = reqwest::Client::new();
 
-    // Act
     let response = client
         .get(format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
 
-    // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
