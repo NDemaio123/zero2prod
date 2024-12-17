fb38148907110d5aea33f7cff38a60099aa8f3b0
Nicholas Demaio <ndemaio123@proton.me>
2024-12-17 22:15:15 +1000

    feat(tracing): use tracing-actix-web tracing middleware

--- src/startup.rs
+++ src/startup.rs
@@ -3,11 +3,11 @@ use std::net::TcpListener;
 
 use actix_web::{
     dev::Server,
-    middleware::Logger,
     web::{self},
     App, HttpServer,
 };
 use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 
 /// The entry point to for the web server
 ///
@@ -17,7 +17,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
