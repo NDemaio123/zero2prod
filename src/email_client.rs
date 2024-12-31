5323112729deaefcde460fdb3443f3e448fcf976
Nicholas Demaio <ndemaio123@proton.me>
2024-12-31 14:03:51 +1000

    test(email): test with more constraints

--- src/email_client.rs
+++ src/email_client.rs
@@ -31,7 +31,7 @@ impl EmailClient {
         subject: &str,
         html_content: &str,
         text_content: &str,
-    ) -> Result<(), reqwest::Error> {
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let url = self.base_url.to_owned();
         let request_body = SendEmailRequest {
             from: self.sender.as_ref().to_owned(),
@@ -40,6 +40,7 @@ impl EmailClient {
             html_body: html_content.to_owned(),
             text_body: text_content.to_owned(),
         };
+        let url = url.join("/email")?;
         self.http_client
             .post(url)
             .header(
@@ -54,6 +55,7 @@ impl EmailClient {
 }
 
 #[derive(serde::Serialize)]
+#[serde(rename_all = "PascalCase")]
 struct SendEmailRequest {
     from: String,
     to: String,
@@ -70,8 +72,27 @@ mod test {
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
     use secrecy::SecretString;
-    use wiremock::matchers::any;
+    use wiremock::matchers::{header, header_exists, method, path};
     use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    struct SendEmailBodyMatcher;
+
+    impl wiremock::Match for SendEmailBodyMatcher {
+        fn matches(&self, request: &wiremock::Request) -> bool {
+            let result: Result<serde_json::Value, _> = serde_json::from_slice(&request.body);
+            match result {
+                Ok(body) => {
+                    dbg!(&body);
+                    body.get("From").is_some()
+                        && body.get("To").is_some()
+                        && body.get("Subject").is_some()
+                        && body.get("HtmlBody").is_some()
+                        && body.get("TextBody").is_some()
+                }
+                Err(_) => false,
+            }
+        }
+    }
 
     #[tokio::test]
     async fn send_email_fires_a_request_to_base_url() {
@@ -84,7 +105,11 @@ mod test {
             SecretString::from(authorization_token),
         );
 
-        Mock::given(any())
+        Mock::given(header_exists("X-Postmark-Server-Token"))
+            .and(header("Content-Type", "application/json"))
+            .and(path("/email"))
+            .and(method("POST"))
+            .and(SendEmailBodyMatcher)
             .respond_with(ResponseTemplate::new(200))
             .expect(1)
             .mount(&mock_server)
